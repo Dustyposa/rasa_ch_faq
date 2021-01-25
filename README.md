a5beeb0be381450cab1a0f784583ac7d74f09432
dustyposa <zmzposa@outlook.com>
2021-01-25 13:52:44 +0800

    :tada: project init

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # rasa_ch_faq
+用 `RASA` 实现 `RASA FAQ`。 回答关于 `RASA` 的问题。
+
+欢迎大家多提 `RASA` 相关的问题，我会补充在这里。
+
+
+## 支持的问题列表
+请参见: [问题列举](./data/nlu/rasa_faq.yml)
+
+## 一些配置
+分词使用的 `bert`， 自定义了 [`tokenizers`](./piplines/tokenizers.py) 
---
+++ piplines/tokenizers.py
@@ -0,0 +1,23 @@
+from typing import List, Text, Dict, Any
+
+from rasa.shared.nlu.training_data.message import Message
+from transformers import AutoTokenizer
+from rasa.nlu.tokenizers.tokenizer import Tokenizer, Token
+
+
+class CustomBertTokenizer(Tokenizer):
+
+    def __init__(self, component_config: Dict[Text, Any] = None) -> None:
+        super(CustomBertTokenizer, self).__init__(component_config)
+        self.tokenizer = AutoTokenizer.from_pretrained(
+            self.component_config["model_weights"], cache_dir=self.component_config.get("cache_dir"), use_fast=True
+        )
+
+    def tokenize(self, message: Message, attribute: Text) -> List[Token]:
+        text = message.get(attribute)
+        encoded_input = self.tokenizer(text, return_offsets_mapping=True, add_special_tokens=False)
+        token_position_pair = zip(encoded_input.tokens(), encoded_input["offset_mapping"])
+
+        return [Token(text=token_text, start=position[0], end=position[1])
+                for token_text, position in token_position_pair]
+
