3977686f6611bf0d515def75a71cff4cc51bd8fd
dustyposa <zmzposa@outlook.com>
2021-01-28 17:21:04 +0800

    :wrench:  del 无用代码

--- run.py
+++ run.py
@@ -1,10 +1,7 @@
 import rasa
 
-from channels.channel import CustomInput
-
 rasa.run(
     model="models",
     endpoints="endpoints.yml",
-    # connector="channels.channel.CustomInput"
     credentials="credentials.yml"
 )
