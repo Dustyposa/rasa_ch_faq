59d89a2f26b174f0940e5e09a5018e9b131c2050
dustyposa <zmzposa@outlook.com>
2021-05-06 18:22:02 +0800

    :star: 增加找饭店 stories

--- actions/actions.py
+++ actions/actions.py
@@ -9,7 +9,7 @@
 import random
 from typing import Any, Text, Dict, List
 #
-from rasa_sdk import Action, Tracker
+from rasa_sdk import Action, Tracker, FormValidationAction
 from rasa_sdk.executor import CollectingDispatcher
 
 import cpca
@@ -56,6 +56,100 @@ class ActionQueryWeather(Action):
         #        'refer': {'sources': ['Weather China'], 'license': ['no commercial use']}}
         # res = res["now"]
         return f"{res['text']} 风向 {res['windDir']}\n温度: {res['temp']} 摄氏度\n体感温度：{res['feelsLike']}"
+
+
+class ValidateRestaurantForm(FormValidationAction):
+    """Example of a form validation action."""
+
+    def name(self) -> Text:
+        return "validate_饭店_form"
+
+    @staticmethod
+    def cuisine_db() -> List[Text]:
+        """Database of supported cuisines."""
+
+        return [
+            "西餐",
+            "中餐",
+            "泰餐",
+            "火锅",
+            "串串",
+            "川菜",
+            "粤菜",
+            "本帮",
+            "麻辣烫",
+            "湘菜",
+            "日料",
+        ]
+
+    @staticmethod
+    def is_int(string: Text) -> bool:
+        """Check if a string is an integer."""
+
+        try:
+            int(string)
+            return True
+        except ValueError:
+            return False
+
+    def validate_cuisine(
+            self,
+            value: Text,
+            dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any],
+    ) -> Dict[Text, Any]:
+        """Validate cuisine value."""
+        print(f"slots: {tracker.current_slot_values()}, value: {value}")
+        if value.lower() in self.cuisine_db():
+            # validation succeeded, set the value of the "cuisine" slot to value
+            return {"cuisine": value}
+        else:
+            dispatcher.utter_message(template="utter_wrong_cuisine")
+            # validation failed, set this slot to None, meaning the
+            # user will be asked for the slot again
+            return {"cuisine": None}
+
+    def validate_num_people(
+            self,
+            value: Text,
+            dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any],
+    ) -> Dict[Text, Any]:
+        """Validate num_people value."""
+        value = next(tracker.get_latest_entity_values("number"), None)
+        if self.is_int(value) and int(value) > 0:
+            return {"num_people": value}
+        else:
+            dispatcher.utter_message(template="utter_wrong_num_people")
+            # validation failed, set slot to None
+            return {"num_people": None}
+
+    def validate_outdoor_seating(
+            self,
+            value: Text,
+            dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any],
+    ) -> Dict[Text, Any]:
+        """Validate outdoor_seating value."""
+
+        if isinstance(value, str):
+            if "外面" in value:
+                # convert "out..." to True
+                return {"outdoor_seating": True}
+            elif "里面" in value:
+                # convert "in..." to False
+                return {"outdoor_seating": False}
+            else:
+                dispatcher.utter_message(template="utter_wrong_outdoor_seating")
+                # validation failed, set slot to None
+                return {"outdoor_seating": None}
+
+        else:
+            # affirm/deny was picked up as True/False by the from_intent mapping
+            return {"outdoor_seating": value}
 
 
 if __name__ == '__main__':
